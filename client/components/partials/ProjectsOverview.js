2882de2df3a411c20f649f3003bb7fef81ab5719
Adrien Vanderpotte <adrien.vanderpotte@gmail.com>
2016-11-16 19:48:30 +0100

    Clean projects overview

--- client/components/partials/ProjectsOverview.js
+++ client/components/partials/ProjectsOverview.js
@@ -63,7 +63,6 @@ export default class ProjectsOverview extends BaseComponent {
   }
   pageTransitionOut() {
     const newRoute = Router.getNewRoute()
-    console.log(newRoute)
     const state = {
       currentPage: newRoute.type.toLowerCase()
     }
