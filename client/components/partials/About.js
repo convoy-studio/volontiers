c4f5ed89915d4cbc2b19f4bf93efc0785e71e82d
Adrien Vanderpotte <adrien.vanderpotte@gmail.com>
2016-12-20 14:32:31 +0100

    Update about

--- client/components/partials/About.js
+++ client/components/partials/About.js
@@ -2,6 +2,7 @@ import BaseComponent from '../../pager/components/BaseComponent'
 import Store from '../../store'
 import Actions from '../../actions'
 import Constants from '../../constants'
+import MainTitle from '../../components/partials/MainTitle'
 import dom from 'dom-hand'
 
 export default class About extends BaseComponent {
@@ -11,7 +12,10 @@ export default class About extends BaseComponent {
     this.toggleOverlay = this.toggleOverlay.bind(this)
     this.hideOverlay = this.hideOverlay.bind(this)
     this.toggleAbout = this.toggleAbout.bind(this)
+    this.toggleList = this.toggleList.bind(this)
+    this.isMobile = Store.Detector.isMobile
     this.hidden = true
+    this.listHidden = true
     Store.on(Constants.TOGGLE_ABOUT, this.toggleOverlay)
     Store.on(Constants.OPEN_PROJECTS_OVERVIEW, this.onOverviewOpen)
     Store.on(Constants.ROUTE_CHANGED, this.hideOverlay)
@@ -20,7 +24,7 @@ export default class About extends BaseComponent {
     const content = Store.getContent('about')
     return (
       <div id='about-page' ref='page-wrapper' className='page-wrapper' onClick={this.toggleAbout}>
-        <div className='wrapper'>
+        <div className='wrapper' ref='wrapper'>
           <div className='description' ref='description'>
             <p dangerouslySetInnerHTML={{__html: content.text[0]}}></p>
             <p dangerouslySetInnerHTML={{__html: content.text[1]}}></p>
@@ -28,12 +32,16 @@ export default class About extends BaseComponent {
           </div>
           <p className='catchline' ref='catchline'>{content.text[3]}</p>
           <div className='details' ref='details'>
-            <p><a className='link btn' href='https://www.google.fr/maps/place/14+Rue+Coquillière,+75001+Paris' target='_blank'>14 rue coquillière 75001 Paris - France</a></p>
-            <p>+ 33 (0) 1 53 69 63 87 | <a className='link btn mail' href='mailto:contact@volontiers.fr' ref='mail'>contact@volontiers.fr</a></p>
+            <p><a className='link btn' href='https://www.google.fr/maps/place/14+Rue+Coquillière,+75001+Paris' target='_blank'>14 rue Coquillière 75001 Paris - France</a></p>
+            <p>+ 33 (0) 1 53 69 63 83 | <a className='link btn mail' href='mailto:hello@volontiers.fr' ref='mail'>hello@volontiers.fr</a></p>
+          </div>
+          <div className='credits' ref='credits'>
+            <MainTitle ref='creditsBtn' title={'Credits'} hasMouseEnterLeave={true} onClick={this.toggleList} className='link credits__btn'></MainTitle>
+            <p className='credits__list' ref='list'> Design: <a className='link btn' href='https://vimeo.com/volontiersproduction' target='_blank'>M/M Paris</a>, Website: <a className='link btn' href='https://convoy.me' target='_blank'>Convoy</a>, Photography: <a className='link btn' href='https://convoy.me' target='_blank'>Henry Paul</a></p>
           </div>
         </div>
         <div className='rs' ref='rs'>
-          <p><a className='link btn' href='https://www.google.fr' target='_blank' ref='fb'>Facebook</a> | <a className='link btn' href='https://www.google.fr' target='_blank' ref='insta'>Instagram</a></p>
+          <p><a className='link btn' href='https://www.facebook.com/volontiersparis/' target='_blank' ref='fb'>Facebook</a> | <a className='link btn' href='https://www.instagram.com/volontiers_paris/' target='_blank' ref='insta'>Instagram</a> | <a className='link btn' href='https://vimeo.com/volontiersproduction' target='_blank' ref='vimeo'>Vimeo</a></p>
         </div>
       </div>
     )
@@ -51,30 +59,47 @@ export default class About extends BaseComponent {
     this.tlBlink.to(this.refs.fb, lengthBlink, { color: '#000000', ease: Sine.easeInOut }, 3 * lengthBlink )
     this.tlBlink.to(this.refs.insta, lengthBlink, { color: '#f7a1fa', ease: Sine.easeInOut }, (4 * lengthBlink) - 0.1 )
     this.tlBlink.to(this.refs.insta, lengthBlink, { color: '#000000', ease: Sine.easeInOut }, 5 * lengthBlink )
+    this.tlBlink.to(this.refs.vimeo, lengthBlink, { color: '#f7a1fa', ease: Sine.easeInOut }, (6 * lengthBlink) - 0.1)
+    this.tlBlink.to(this.refs.vimeo, lengthBlink, { color: '#000000', ease: Sine.easeInOut }, 7 * lengthBlink )
     this.tlBlink.pause(0)
     this.tlOverlayIn = new TimelineMax({ onComplete: () => {
       dom.classes.add(this.parent, 'show')
       this.tlBlink.play(0)
     }})
     this.tlOverlayIn.set(this.parent, { visibility: 'visible' }, 0)
+    this.tlOverlayIn.set(this.refs.list, { opacity: '0' }, 0)
     this.tlOverlayIn.fromTo(this.parent, 0.55, { opacity: 0 }, { opacity: 1, force3D: true, ease: Expo.easeOut }, 0)
     this.tlOverlayIn.to(dom.select('#canvas-container'), 0.5, {backgroundColor: '#ffffff' }, 0)
     this.tlOverlayIn.staggerFromTo(this.refs.description.children, 0.7, { opacity: 0 }, { opacity: 1, ease: Sine.easeInOut }, 0.2, 0.5)
     this.tlOverlayIn.fromTo(this.refs.catchline, 0.55, { opacity: 0, y: 5 }, { opacity: 1, y: 0, ease: Sine.easeInOut }, 1)
     this.tlOverlayIn.fromTo(this.refs.details, 0.55, { opacity: 0, y: 5 }, { opacity: 1, y: 0, ease: Sine.easeInOut }, 1.2)
+    this.tlOverlayIn.addCallback(this.refs.creditsBtn.show, 1.4)
     this.tlOverlayIn.fromTo(this.refs.rs, 0.55, { opacity: 0, y: 5 }, { opacity: 1, y: 0, ease: Sine.easeInOut }, 1.4)
     this.tlOverlayIn.pause(0)
 
     this.tlOverlayOut = new TimelineMax({ onComplete: () => {
       dom.classes.remove(this.parent, 'show')
+      this.refs.creditsBtn.hide()
       TweenMax.set(this.parent, { visibility: 'hidden' })
     }})
     this.tlOverlayOut.fromTo(this.parent, 0.5, { opacity: 1 }, { opacity: 0, force3D: true, ease: Expo.easeOut }, 0)
     this.tlOverlayOut.pause(0)
   }
   toggleAbout(e) {
-    if (dom.classes.has(e.target, 'link')) return
+    if (dom.classes.has(e.target, 'link') || dom.classes.has(e.target, 'title')) return
     setTimeout(() => { Actions.toggleAbout() })
+  }
+  toggleList() {
+    if (this.listHidden) {
+      TweenMax.set(this.refs.list, { 'display': 'inline-block' })
+      TweenMax.fromTo(this.refs.list, 0.6, { opacity: 0, y: 10 }, { opacity: 1, y: 0, ease: Expo.easeOut })
+      if (this.isMobile) TweenMax.to(this.refs.wrapper, 1, { scrollTop: this.refs.list.getBoundingClientRect().top })
+      this.listHidden = false
+    } else {
+      TweenMax.set(this.refs.list, { 'display': 'none' })
+      TweenMax.fromTo(this.refs.list, 0.6, { opacity: 1, y: 0 }, { opacity: 0, y: 10, ease: Expo.easeOut })
+      this.listHidden = true
+    }
   }
   toggleOverlay() {
     if (this.hidden) {
@@ -87,7 +112,9 @@ export default class About extends BaseComponent {
   }
   hideOverlay() {
     this.hidden = true
+    this.listHidden = true
     TweenMax.set(this.parent, { opacity: 0 })
+    TweenMax.set(this.refs.list, { 'display': 'none' })
     if (dom.classes.has(this.parent, 'show')) dom.classes.remove(this.parent, 'show')
   }
   onOverviewOpen() {
