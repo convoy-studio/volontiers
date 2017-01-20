3f974fc1059898c21f23283fbe8dd8e4b9912da1
Adrien Vanderpotte <adrien.vanderpotte@gmail.com>
2017-01-20 10:52:12 +0100

    Fix sldie mobile detect

--- client/components/partials/Slide.js
+++ client/components/partials/Slide.js
@@ -22,13 +22,13 @@ const transitionShowBezier = bezier(1, 0.13, 0.7, 0.89, 500)
 let transitionHideTime = 0
 let transitionShowTime = 0
 let playing = true
-const isMobile = Store.Detector.isMobile
 
 export default (id, container, imgFilename, index, pre = 'preview', direction = { from: Constants.RIGHT, to: Constants.CENTER }, defaultPosition = Constants.CENTER)=> {
   let scope
   let ext = ''
   const initial = {}
   let currentVideoTime = 0
+  const isMobile = Store.Detector.isMobile
   const createPlane = (texture) => {
     initial.texture = texture
     const plane = {}
@@ -248,7 +248,6 @@ export default (id, container, imgFilename, index, pre = 'preview', direction = 
     Store.on(Constants.CLOSE_PROJECTS_OVERVIEW, onProjectsOverviewClose)
     Store.on(Constants.CHANGE_PROJECTS_PREVIEW, onProjectsPreviewChange)
     Store.on(Constants.TOGGLE_ABOUT, onAboutToggle)
-    console.log(ext)
     if (ext === 'mp4') {
       scope.mesh.texture.baseTexture.source.currentTime = currentVideoTime
       scope.mesh.texture.baseTexture.source.play()
